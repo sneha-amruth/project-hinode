34b594ad1bd63242921fc113ebefdbb4e6759cc7
sneha.amruth <sneha.amruth@servicenow.com>
2021-03-27 15:55:19 +0530

    imported css file

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,30 +1,30 @@
-    var modal = document.getElementById("modal-id");
-    var btn = document.getElementById("modal-btn");
-    var btnCancel = document.getElementById("modal-btn-cancel");
-    var btnDelete = document.getElementById("modal-btn-delete");
+  var modal = document.getElementById("modal-id");
+  var btn = document.getElementById("modal-btn");
+  var btnCancel = document.getElementById("modal-btn-cancel");
+  var btnDelete = document.getElementById("modal-btn-delete");
 
-    var span = document.getElementsByClassName("close")[0];
+  var span = document.getElementsByClassName("close")[0];
 
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
 
-    span.onclick = function() {
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  btnCancel.onclick = function() {
       modal.style.display = "none";
     }
 
-    btnCancel.onclick = function() {
-        modal.style.display = "none";
-      }
-
-      btnDelete.onclick = function() {
-        modal.style.display = "none";
-      }
-
-
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
+    btnDelete.onclick = function() {
+      modal.style.display = "none";
     }
+
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 
